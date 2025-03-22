49cdc7c6237a60652dd0b3e2247892f5c99ceab0
Soumyajit-io <soumyajit010805@gmail.com>
2025-03-22 20:50:14 +0530

    code added

---
+++ basics/02_variable.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    int x = 10;
+    float y = 10.5;
+    char v = 'm';
+    short s = 3200;
+    long l = 555000;
+    long long ll = 2332556652;
+    printf("%d\n",x);
+    printf("%f\n",y);
+    printf("%c\n",v);
+    printf("%d\n",s);
+}
---
+++ basics/03_input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a num: ");  // Display prompt separately
+    scanf("%d", &n);          // Correct format for input
+
+    printf("You entered: %d\n", n);
+    return 0;
+}
