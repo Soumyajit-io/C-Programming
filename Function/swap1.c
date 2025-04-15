544d5341bb7a168c9bfe517203bc8c63817cfb4d
Soumyajit-io <soumyajit010805@gmail.com>
2025-04-15 16:17:26 +0530

    Code  added

---
+++ Function/swap1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int swap(int a , int b){
+    int temp =a;
+    a=b;
+    b=temp;
+    printf("After swapping the value of first number is : %d\n",a);
+    printf("After swapping the value of Second number is : %d\n",b);
+}
+int main (){
+    int a , b ;
+    printf("Enter the first number: \n");
+    scanf("%d", &a);
+    printf("Enter the Second number: \n");
+    scanf("%d", &b);
+    swap(a,b);    
+}
